09987031616c3e6ab01baf3f4554910a01db93ac
Madhin Hussain <madhinhussain@gmail.com>
2023-01-29 22:48:14 +0000

    nodejs server added

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ const fs = require('fs')
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res, err) => {
+  if (err) {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('404: File not found');
+  } else {
   res.writeHead(200, { 'content-type': 'application/javascript' })
   fs.createReadStream('indexTest1.js').pipe(res)
   fs.createReadStream('indexTest2.js').pipe(res)
+  }
 })
 
 server.listen(port, hostname, () => {
